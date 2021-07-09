e9163c96d5d9ec38b643745738869e4b2a707d5d
Paulo Faria <paulo.faria.rl@gmail.com>
2021-07-09 13:32:55 -0300

    Add websocket

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,15 @@ use serde::{Deserialize, Serialize};
 use std::env;
 use std::sync::Mutex;
 use std::path::PathBuf;
+use actix::{Addr, Actor};
+use std::time::Instant;
+use actix_web_actors::ws;
+use crate::websocket_session_actor::{WebsocketSessionActor, MAIN_ROOM};
 
-struct AppState {
-    requested_song_ids: Mutex<Vec<String>>,
-}
+mod websocket_server_actor;
+mod websocket_session_actor;
 
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct AddSongRequestRequest {
-    song_id: String,
-}
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct DeleteSongRequestRequest {
-    song_id: String,
-}
-
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-struct ListSongRequestsResponse {
+pub struct AppState {
     requested_song_ids: Vec<String>,
 }
 
@@ -39,71 +28,135 @@ pub struct SongRequestError {
 impl error::ResponseError for SongRequestError {}
 
 #[get("/")]
-async fn index() -> Result<NamedFile> {
+async fn index_service() -> Result<NamedFile> {
     let path: PathBuf = "songs.json".parse().unwrap();
     Ok(NamedFile::open(path)?)
 }
 
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct ListSongRequestsResponse {
+    requested_song_ids: Vec<String>,
+}
+
 #[get("/song-request")]
-async fn list_song_requests(
-    state: web::Data<AppState>
+async fn list_song_requests_service(
+    state: web::Data<Mutex<AppState>>
 ) -> Result<web::Json<ListSongRequestsResponse>> {
-    let guard = state.requested_song_ids.lock().unwrap();
-    let requested_song_ids = (*guard).clone();
-    Ok(web::Json(ListSongRequestsResponse { requested_song_ids }))
+    let state = state.lock().unwrap();
+
+    Ok(web::Json(ListSongRequestsResponse {
+        requested_song_ids: state.requested_song_ids.clone()
+    }))
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct CreateSongRequestRequest {
+    song_id: String,
 }
 
 #[post("/song-request")]
-async fn add_song_request(
-    add_song_request: web::Json<AddSongRequestRequest>,
-    state: web::Data<AppState>
+async fn create_song_request_service(
+    create_song_request: web::Json<CreateSongRequestRequest>,
+    app_state: web::Data<Mutex<AppState>>,
+    websocket_server_actor_address: web::Data<Addr<websocket_server_actor::WebsocketServerActor>>,
 ) -> Result<web::Json<ListSongRequestsResponse>, SongRequestError> {
-    let mut guard = state.requested_song_ids.lock().unwrap();
+    let mut state = app_state.lock().unwrap();
 
-    return if let None = (*guard).iter().position(|id| *id == add_song_request.song_id) {
-        (*guard).push(add_song_request.song_id.clone());
-        Ok(web::Json(ListSongRequestsResponse { requested_song_ids: (*guard).clone() }))
+    let position = state.requested_song_ids
+        .iter()
+        .position(|id| *id == create_song_request.song_id);
+
+    return if let None = position {
+        state.requested_song_ids.push(create_song_request.song_id.clone());
+
+        websocket_server_actor_address.do_send(
+            websocket_server_actor::BroadcastAppStateMessage {
+                room_name: MAIN_ROOM.to_owned(),
+            }
+        );
+
+        Ok(web::Json(ListSongRequestsResponse {
+            requested_song_ids: state.requested_song_ids.clone()
+        }))
     } else {
         Err(SongRequestError { description: "Existing song id." })
     }
 }
 
-#[delete("/song-request")]
-async fn delete_song_request(
-    delete_song_request: web::Json<DeleteSongRequestRequest>,
-    state: web::Data<AppState>
-) -> Result<web::Json<ListSongRequestsResponse>, SongRequestError> {
-    let mut guard = state.requested_song_ids.lock().unwrap();
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct DeleteSongRequestRequest {
+    song_id: String,
+}
 
-    return if let Some(position) = (*guard).iter().position(|id| *id == delete_song_request.song_id) {
-        (*guard).remove(position);
-        Ok(web::Json(ListSongRequestsResponse { requested_song_ids: (*guard).clone() }))
+#[delete("/song-request")]
+async fn delete_song_request_service(
+    delete_song_request: web::Json<DeleteSongRequestRequest>,
+    state: web::Data<Mutex<AppState>>
+) -> Result<web::Json<ListSongRequestsResponse>, SongRequestError> {
+    let mut state = state.lock().unwrap();
+
+    let position = state.requested_song_ids
+        .iter()
+        .position(|id| *id == delete_song_request.song_id);
+
+    return if let Some(position) = position {
+        state.requested_song_ids.remove(position);
+
+        Ok(web::Json(ListSongRequestsResponse {
+            requested_song_ids: state.requested_song_ids.clone()
+        }))
     } else {
         Err(SongRequestError { description: "Invalid song id." })
     }
 }
 
+async fn websocket_service(
+    request: HttpRequest,
+    stream: web::Payload,
+    websocket_server_actor_address: web::Data<Addr<websocket_server_actor::WebsocketServerActor>>,
+) -> Result<HttpResponse, Error> {
+    ws::start(
+        WebsocketSessionActor {
+            session_id: 0,
+            last_heartbeat: Instant::now(),
+            room_name: MAIN_ROOM.to_owned(),
+            websocket_server_actor_address: websocket_server_actor_address.get_ref().clone(),
+        },
+        &request,
+        stream,
+    )
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
     let port = env::var("PORT")
         .unwrap_or_else(|_| "8080".to_string())
         .parse()
         .expect("PORT must be a number");
 
-    let state = web::Data::new(AppState {
-        requested_song_ids: Mutex::new(vec![]),
-    });
+    let app_state = web::Data::new(Mutex::new(AppState {
+        requested_song_ids: vec![],
+    }));
+
+    let websocket_server_actor_address = websocket_server_actor::WebsocketServerActor::new(app_state.clone()).start();
 
     HttpServer::new(move || {
         let cors = Cors::permissive();
 
         App::new()
             .wrap(cors)
-            .app_data(state.clone())
-            .service(index)
-            .service(list_song_requests)
-            .service(add_song_request)
-            .service(delete_song_request)
+            .app_data(app_state.clone())
+            .data(websocket_server_actor_address.clone())
+            .service(index_service)
+            .service(list_song_requests_service)
+            .service(create_song_request_service)
+            .service(delete_song_request_service)
+            .service(web::resource("/ws/").to(websocket_service))
     })
         .bind(("0.0.0.0", port))?
         .run()
---
+++ src/websocket_session_actor.rs
@@ -0,0 +1,188 @@
+use std::time::{Duration, Instant};
+
+use actix::*;
+use actix_web_actors::ws;
+use crate::websocket_server_actor;
+
+pub const MAIN_ROOM: &str = "main";
+
+pub struct WebsocketSessionActor {
+    /// Unique client session id.
+    pub session_id: usize,
+    /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
+    /// otherwise we drop the connection.
+    pub last_heartbeat: Instant,
+    /// Room the client session is in.
+    pub room_name: String,
+    /// Websocket server actor address.
+    pub websocket_server_actor_address: Addr<websocket_server_actor::WebsocketServerActor>,
+}
+
+impl Actor for WebsocketSessionActor {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, websocket_context: &mut Self::Context) {
+        self.check_heartbeat(websocket_context);
+        let websocket_session_actor_address = websocket_context.address();
+
+        self.websocket_server_actor_address
+            .send(websocket_server_actor::ConnectMessage {
+                websocket_session_actor_recipient: websocket_session_actor_address.recipient(),
+            })
+            .into_actor(self)
+            .then(|result, websocket_session_actor, websocket_context| {
+                match result {
+                    Ok(session_id) => websocket_session_actor.session_id = session_id,
+                    _ => websocket_context.stop(),
+                }
+
+                fut::ready(())
+            })
+            .wait(websocket_context);
+    }
+
+    fn stopping(&mut self, _: &mut Self::Context) -> Running {
+        self.websocket_server_actor_address.do_send(
+            websocket_server_actor::DisconnectMessage { websocket_session_id: self.session_id }
+        );
+
+        Running::Stop
+    }
+}
+
+/// How often heartbeat pings are sent.
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+/// How long before lack of client response causes a timeout.
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+impl WebsocketSessionActor {
+    fn check_heartbeat(&self, context: &mut ws::WebsocketContext<Self>) {
+        context.run_interval(HEARTBEAT_INTERVAL, |websocket_session_actor, websocket_context| {
+            if Instant::now().duration_since(websocket_session_actor.last_heartbeat) > CLIENT_TIMEOUT {
+                log::debug!("Client session heartbeat failed, disconnecting!");
+
+                websocket_session_actor.websocket_server_actor_address.do_send(
+                    websocket_server_actor::DisconnectMessage {
+                        websocket_session_id: websocket_session_actor.session_id
+                    }
+                );
+
+                return websocket_context.stop();
+            }
+
+            log::debug!("Sent ping message to client with session id {}.", websocket_session_actor.session_id);
+            websocket_context.ping(b"");
+        });
+    }
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebsocketSessionActor {
+    fn handle(
+        &mut self,
+        websocket_message: Result<ws::Message, ws::ProtocolError>,
+        websocket_context: &mut Self::Context,
+    ) {
+        let websocket_message = match websocket_message {
+            Ok(websocket_message) => websocket_message,
+            Err(_) => {
+                return websocket_context.stop();
+            }
+        };
+
+        match websocket_message {
+            ws::Message::Ping(ping_message) => {
+                log::debug!("Received ping message from client with session id {}.", self.session_id);
+                self.last_heartbeat = Instant::now();
+                websocket_context.pong(&ping_message);
+            }
+            ws::Message::Pong(_) => {
+                log::debug!("Received pong message from client with session id {}.", self.session_id);
+                self.last_heartbeat = Instant::now();
+            }
+            ws::Message::Text(text_message) => {
+                log::debug!("Received text message from client with session id {}: {}", self.session_id, text_message);
+                let trimmed_message = text_message.trim();
+
+                if trimmed_message.starts_with('/') {
+                    let words: Vec<&str> = trimmed_message
+                        .splitn(2, ' ')
+                        .collect();
+
+                    match words[0] {
+                        "/list" => {
+                            log::debug!("Received /list message");
+
+                            self.websocket_server_actor_address
+                                .send(websocket_server_actor::ListRoomsMessage)
+                                .into_actor(self)
+                                .then(|result, _, websocket_context| {
+                                    match result {
+                                        Ok(rooms) => {
+                                            for room in rooms {
+                                                websocket_context.text(room);
+                                            }
+                                        }
+                                        _ => log::error!("Websocket server actor failed to respond to /list command."),
+                                    }
+                                    fut::ready(())
+                                })
+                                .wait(websocket_context)
+                        }
+                        "/join" => {
+                            log::debug!("Received /join message");
+
+                            if words.len() == 2 {
+                                self.room_name = words[1].to_owned();
+
+                                self.websocket_server_actor_address.do_send(websocket_server_actor::JoinMessage {
+                                    session_id: self.session_id,
+                                    room_name: self.room_name.clone(),
+                                });
+
+                                websocket_context.text("joined");
+                            } else {
+                                websocket_context.text("!!! room name is required");
+                            }
+                        }
+                        _ => websocket_context.text(format!("!!! unknown command: {:?}", trimmed_message)),
+                    }
+                } else {
+                    self.websocket_server_actor_address.do_send(websocket_server_actor::ClientMessage {
+                        session_id: self.session_id,
+                        message: trimmed_message.to_owned(),
+                        room_name: self.room_name.clone(),
+                    })
+                }
+            }
+            ws::Message::Binary(_) => log::error!("Unexpected binary websocket message."),
+            ws::Message::Close(close_reason) => {
+                if let Some(close_reason) = &close_reason {
+                    log::debug!("Received close message from client with session id {}: {:?}", self.session_id, close_reason);
+                } else {
+                    log::debug!("Received close message from client with session id {}.", self.session_id);
+                }
+                websocket_context.close(close_reason);
+                websocket_context.stop();
+            }
+            ws::Message::Continuation(_) => {
+                log::debug!("Received continuation message from client with session id {}.", self.session_id);
+                websocket_context.stop();
+            }
+            ws::Message::Nop => (),
+        }
+    }
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct WebsocketReplyMessage {
+    pub message: String,
+}
+
+impl Handler<WebsocketReplyMessage> for WebsocketSessionActor {
+    type Result = ();
+
+    fn handle(&mut self, websocket_reply_message: WebsocketReplyMessage, websocket_context: &mut Self::Context) {
+        websocket_context.text(websocket_reply_message.message);
+    }
+}
